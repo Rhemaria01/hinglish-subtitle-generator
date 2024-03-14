a81672daf9b4b4fd23b1fcd8571013575cf1b878
Rhemaria01 <reenavhemaria789@gmail.com>
2024-03-14 14:25:44 +0530

    Refactoring and srtToVid function

--- index.js
+++ index.js
@@ -3,29 +3,66 @@ const {
   StartTranscriptionJobCommand,
   GetTranscriptionJobCommand,
 } = require("@aws-sdk/client-transcribe");
-
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const { OpenAI } = require("openai");
 const region = "ap-southeast-1";
-const fs = require("fs");
 
+const {
+  getObjectURL,
+  getFile,
+  timeout,
+  parseSRT,
+  addToFile,
+  createDir,
+} = require("./utils");
 require("dotenv").config({ override: true });
+const ffmpeg = require("fluent-ffmpeg");
 
 const credentials = {
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
 };
 
-//file names to use
+//testing files
 
-// TranscriptionJobName: hin_eng_min_transcription_srtFormat,  Uri: hin_eng_min.mp4
-// TranscriptionJobName: video_02_transcription_srtFormat,  Uri: standup_video_4mins.mp4
+const testingFiles = [
+  {
+    index: 0,
+    TranscriptionJobName: "hin_eng_min_transcription_srtFormat",
+    videoFileKey: "hin_eng_min.mp4",
+  },
+  {
+    index: 1,
+    TranscriptionJobName: "video_02_transcription_srtFormat",
+    videoFileKey: "standup_video_4mins.mp4",
+  },
+  {
+    index: 2,
+    TranscriptionJobName: "full_hindi_14mins_subtitle",
+    videoFileKey: "full_hindi_14mins.mp4",
+  },
+  {
+    index: 3,
+    TranscriptionJobName: "old_movie_clip_9mins_subtitle",
+    videoFileKey: "old_movie_clip_9mins.mp4",
+  },
+  {
+    index: 4,
+    TranscriptionJobName: "hin_reactRoadmap_30Mins_subtitle",
+    videoFileKey: "hin_reactRoadmap_30Mins.mp4",
+  },
+];
+
+//element of testing files
+let testIndex = 4;
+
+//video file and subtitle names
+const videoFileKey = testingFiles[testIndex].videoFileKey;
+const hinglishSubFileName = `hinglish_srt/${testingFiles[testIndex].TranscriptionJobName}.srt`;
 
 const input = {
-  TranscriptionJobName: "video_02_transcription_srtFormat",
+  TranscriptionJobName: testingFiles[testIndex].TranscriptionJobName,
   Media: {
-    MediaFileUri: "s3://transcription-xyz/standup_video_4mins.mp4",
+    MediaFileUri: `s3://transcription-xyz/${videoFileKey}`,
   },
   OutputBucketName: "transcription-xyz",
   OutputKey: "transcriptions/",
@@ -36,25 +73,50 @@ const input = {
     OutputStartIndex: 1,
   },
 };
-const fileKey = `${input.OutputKey}${input.TranscriptionJobName}`;
-const completionStatus = ["COMPLETED", "FAILED"];
 
+//name of subtitle file on S3
+const fileKey = `${input.OutputKey}${input.TranscriptionJobName}`;
+
+async function main() {
+  //if job already exists in aws than skip using startTranscription function and use this instead:
+  // const srtFile = await getFile(`${fileKey}.srt`, region, credentials);
+
+  const srtFile = await startTranscriptionRequest();
+
+  console.log("Sending original transcription to openai.");
+  const hinglishTranscription = await getHinglishTranscription(srtFile);
+
+  // adding sorted hinglish transcription to subtitle file
+  addToFile(hinglishSubFileName, hinglishTranscription);
+  await timeout(1000);
+
+  console.log("Adding subtitles to video");
+  await addSrtToVideo(hinglishSubFileName);
+
+  console.log("Completed");
+}
+
+// Get the video file from S3 Bucket and Start a transcription job for that file
 async function startTranscriptionRequest() {
-  console.log("Starting");
+  console.log("Starting Transcription");
   const transcribeConfig = {
     region,
     credentials,
   };
   const transcribeClient = new TranscribeClient(transcribeConfig);
   const transcribeCommand = new StartTranscriptionJobCommand(input);
+  const completionStatus = ["COMPLETED", "FAILED"];
+
   try {
     const transcribeResponse = await transcribeClient.send(transcribeCommand);
     console.log("Transcription job created, the details:");
     console.log(transcribeResponse.TranscriptionJob);
   } catch (err) {
     console.log(err);
+    throw new Error(err);
   }
 
+  //polling for transcription job to end
   while (true) {
     const getTranscriptionCommand = new GetTranscriptionJobCommand({
       TranscriptionJobName: input.TranscriptionJobName,
@@ -63,90 +125,97 @@ async function startTranscriptionRequest() {
     if (
       completionStatus.includes(status.TranscriptionJob.TranscriptionJobStatus)
     ) {
-      // const jsonFile = await getFile(`${fileKey}.json`);
-      const srtFile = await getFile(`${fileKey}.srt`);
-      getHinglishTranscription(srtFile);
-      break;
+      console.log("Transcribtion Job Completed, getting srt file from aws");
+
+      //getting the new subtitle file created by aws transcribe
+      const srtFile = await getFile(`${fileKey}.srt`, region, credentials);
+      return srtFile;
     }
     await timeout(5000);
-    console.log("Re-executing...");
+    console.log("Re-checking...");
   }
 }
 
-async function getFile(key) {
-  const url = await getObjectURL(key);
-
-  let res = await fetch(url)
-    .then((res) => res.blob())
-    .then((blob) => blob.text());
-  return res;
-}
-
-async function getObjectURL(key) {
-  const s3Client = new S3Client({
-    region,
-    credentials,
-  });
-  const command = new GetObjectCommand({
-    Bucket: "transcription-xyz",
-    Key: key,
-  });
-
-  const url = await getSignedUrl(s3Client, command);
-  // console.log(url);
-  return url;
-}
-
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 const client = new OpenAI();
+
+//Send the transcription generated by aws to openai to make convert it into hinglish.
 async function getHinglishTranscription(awsTranscription) {
-  awsTranscription = await getFile(`${fileKey}.srt`); // when changed to srt file no need for this step
-  const hinglishTranscription = await client.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    max_tokens: Infinity,
-    messages: [
-      {
-        role: "system",
-        content: `Convert the following transcription provide by user this into Hinglish transcription, Output should be in JSON format so the system can easily just use JSON.parse method on the output provided.
-           Don't make use of code block in your answer just directly send your answer because I am parsing the JSON string.
-           Format the answer as follows: [{"segment": "segment in hinglish","start":"00:00:00.039","end":"00:00:01.159"},...].
-           Input is an srt file so don't miss any segment or it will mess up the time for all the up segments.
-           Convert Hindi Words to Hinglish and let english words be english only.`,
-      },
-      { role: "user", content: awsTranscription },
-    ],
-  });
-  fs.appendFile(
-    "hinglish_transcription.json",
-    hinglishTranscription.choices[0].message.content,
-    (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Successfull");
-      }
-    }
+  // awsTranscription = await readFileToString(
+  //   "transcriptions/full_hindi_14mins_subtitle.srt"
+  // ).then((content) => {
+  //   return content;
+  // });
+  awsTranscription = parseSRT(awsTranscription);
+
+  let count = 0;
+  const result = [];
+  await Promise.all(
+    awsTranscription.map(async (transcription, index) => {
+      count++;
+      console.log("Sending to openai:", count);
+
+      let hinglishTranscription = await client.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `Convert the following transcription provide by user this into Hinglish transcription, Output should be in SRT format.
+Do not use Code block in your answer.   
+Format the answer as follows: 
+1
+00:00:00,119 --> 00:00:00,879
+Hinglish Transcription
+
+2
+00:00:02,700 --> 00:00:03,779
+Hinglish Transcription
+
+3
+00:00:04,119 --> 00:00:04,879
+Hinglish Transcription
+
+4
+00:00:05,700 --> 00:00:05,779
+Hinglish Transcription
+
+Do Not Miss any segment and convert the complete file. 
+Convert Hindi Words to Hinglish and let english words be english only.
+Convert each and every word.
+          `,
+          },
+          {
+            role: "user",
+            content: transcription,
+          },
+        ],
+      });
+
+      result.push({
+        index,
+        srt: hinglishTranscription.choices[0].message.content + "\n\n",
+      });
+    })
   );
-
-  //Streaming response
-  // const writeStream = fs.createWriteStream("hinglish_transcription.json");
-  // for await (const part of hinglishTranscription) {
-  //   writeStream.write(part.choices[0]?.delta?.content ?? "");
-  // }
-  // writeStream.end();
-  // writeStream.on("finish", () => {
-  //   console.log(
-  //     `Streaming GPT output has been written to hinglish_transcription.json`
-  //   );
-  // });
-
-  // writeStream.on("error", (err) => {
-  //   console.error(`Error writing to file: ${err}`);
-  // });
+  console.log(
+    "All the requests are completed, sorting and returning the hinglish subtitles."
+  );
+  return result
+    .sort((a, b) => a.index - b.index)
+    .reduce((acc, { srt }) => acc + srt, "");
 }
-// startTranscriptionRequest();
 
-getHinglishTranscription(""); //change to srt file later
+//Fetches the video from S3 bucket, adds subtitles to it and saves it locally.
+async function addSrtToVideo(srt) {
+  const videoURL = await getObjectURL(videoFileKey, region, credentials);
+
+  ffmpeg(videoURL)
+    .outputOptions(`-vf subtitles=${srt}`)
+    .on("error", function (err) {
+      console.log(err);
+    })
+    .save(`hinglish_videos/Hinglish_${videoFileKey}`);
+}
+
+createDir("hinglish_srt");
+createDir("hinglish_videos");
+main();
---
+++ utils/index.js
@@ -0,0 +1,113 @@
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const fs = require("fs");
+
+//function to get File from S3 bucket
+async function getFile(key, region, credentials) {
+  const url = await getObjectURL(key, region, credentials);
+
+  let res = await fetch(url)
+    .then((res) => res.blob())
+    .then((blob) => blob.text());
+  return res;
+}
+
+// get object url of an object in s3 bucket using it's key
+async function getObjectURL(key, region, credentials) {
+  const s3Client = new S3Client({
+    region,
+    credentials,
+  });
+  const command = new GetObjectCommand({
+    Bucket: "transcription-xyz",
+    Key: key,
+  });
+
+  const url = await getSignedUrl(s3Client, command);
+  // console.log(url);
+  return url;
+}
+
+//fake timeout function
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// add output to file to view it
+async function addToFile(filename, data) {
+  fs.appendFile(filename, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfull added to:", filename);
+    }
+  });
+}
+
+//check if directory exists and create if it doesn't
+function createDir(dirName) {
+  if (!fs.existsSync(dirName)) {
+    fs.mkdirSync(dirName);
+  }
+}
+
+//function to convert json to srt and add it to hingligh.srt file
+async function jsonToSrt(json, srtFileName) {
+  let srt = "";
+  for (let i = 0; i < json.length; i++) {
+    const { segment, start, end } = json[i];
+    let data = `${i + 1}
+${start.replace(".", ",")} --> ${end.replace(".", ",")}
+${segment}
+
+`;
+    srt += data;
+    await addToFile(srtFileName, data);
+    await timeout(2);
+  }
+  return srt;
+}
+
+// Converts file content tot string
+function readFileToString(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
+
+//divides srt file in chunks of 50 time segments.
+function parseSRT(srtContent) {
+  const lines = srtContent.trim().split("\n");
+  const transcriptions = [];
+  let combinedText = "";
+  for (let i = 0; i < lines.length; i += 4) {
+    const text = `${lines[i]}
+${lines[i + 1]}
+${lines[i + 2]}`;
+    combinedText += text + "\n\n";
+    if (i !== 0 && i % 200 === 0) {
+      transcriptions.push(combinedText);
+      combinedText = "";
+      continue;
+    }
+  }
+  if (combinedText) transcriptions.push(combinedText);
+  return transcriptions;
+}
+
+module.exports = {
+  getObjectURL,
+  getFile,
+  timeout,
+  addToFile,
+  jsonToSrt,
+  readFileToString,
+  parseSRT,
+  createDir,
+};
